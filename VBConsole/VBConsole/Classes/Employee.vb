87110cb2eefd11cb06d0e2734bd92c820db6a5a5
Carlos Mtz <carlosmdelc@gmail.com>
2022-01-29 19:31:25 -0600

    Added the console project and classes folder with Employee

---
+++ VBConsole/VBConsole/Classes/Employee.vb
@@ -0,0 +1,21 @@
+﻿Public Class Employee
+    Private _Id As String
+    Public Property Id() As String
+        Get
+            Return _Id
+        End Get
+        Set(ByVal value As String)
+            _Id = value
+        End Set
+    End Property
+
+    Private _Name As String
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(ByVal value As String)
+            _Name = value
+        End Set
+    End Property
+End Class
